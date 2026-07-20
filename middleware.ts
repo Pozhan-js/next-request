742719a231aa3519e02c0e35bff753bbbc7a4625
Amos Jach <106759039+Pozhan-js@users.noreply.github.com>
2026-07-20 10:10:04 +0800

    fix: resolve middleware type errors

--- middleware.ts
+++ middleware.ts
@@ -1,5 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
 // 中间件可以是 async 函数，如果使用了 await
-export function middleware(request) {
+export function middleware(request: NextRequest) {
   return NextResponse.redirect(new URL("/home", request.url));
 }
 
